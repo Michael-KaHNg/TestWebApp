ae59f0ffd648529be2d12bee5008ef35efa51820
Michael Ng <68442259+Michael-KaHNg@users.noreply.github.com>
2021-09-19 16:05:20 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+# A simple script to calculate BMI
+from pywebio.input import input, FLOAT
+from pywebio.output import put_text
+from pywebio import start_server
+
+def bmi():
+    height = input("Input your height(cm)：", type=FLOAT)
+    weight = input("Input your weight(kg)：", type=FLOAT)
+
+    BMI = weight / (height / 100) ** 2
+
+    top_status = [(16, 'Severely underweight'), (18.5, 'Underweight'),
+                  (25, 'Normal'), (30, 'Overweight'),
+                  (35, 'Moderately obese'), (float('inf'), 'Severely obese')]
+
+    for top, status in top_status:
+        if BMI <= top:
+            put_text('Your BMI: %.1f. Category: %s' % (BMI, status))
+            break
+
+if __name__ == '__main__':
+    #bmi()   #  run in spyder IDE
+    #pywebio.start_server(bmi, port = 80)  #  run on local computer through terminal
+    start_server(main, debug=True, port=8080, cdn=False)  #  run on cloud
+    
